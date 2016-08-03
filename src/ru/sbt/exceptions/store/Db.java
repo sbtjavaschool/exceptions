67920a6c4598cb72bb2975f65532b44202a2725f
sbtjavaschool <sbt.java.school@gmail.com>
2016-08-03 21:09:53 +0300

    exceptions

--- src/ru/sbt/exceptions/store/Db.java
+++ src/ru/sbt/exceptions/store/Db.java
@@ -1,11 +1,12 @@
 package ru.sbt.exceptions.store;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface Db extends AutoCloseable {
     void insert(String line) throws SQLException;
 
-    void selectAll(String line) throws SQLException;
+    List<String> selectAll() throws SQLException;
 
     void close();
 }
